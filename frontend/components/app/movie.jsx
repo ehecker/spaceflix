604fe2eb4810182ec5c1f8e51a92af26eec2d3ab
ehecker <ezhecker@gmail.com>
2020-05-05 08:24:25 -0700

    Add fade animation to video details

--- frontend/components/app/movie.jsx
+++ frontend/components/app/movie.jsx
@@ -4,16 +4,39 @@ class Movie extends React.Component {
     constructor(props) {
         super(props)
 
+        this.playVideo = this.playVideo.bind(this);
+        this.stopVideo = this.stopVideo.bind(this);
+        this.showThumbnail = this.showThumbnail.bind(this);
+        this.hideThumbnail = this.hideThumbnail.bind(this);
     }
 
+    playVideo(event) {
+        event.currentTarget.play()
+    }
 
+    stopVideo(event) {
+        event.currentTarget.pause()
+    }
 
     showThumbnail(event) {
-
+        event.preventDefault();
+        event.currentTarget.classList.remove("playing")
     }
 
     hideThumbnail(event) {
+        event.preventDefault();
+        event.currentTarget.classList.add("playing")
+    }
 
+    fadeDetails(event) {
+        console.log("Fade function fired")
+        event.currentTarget.classList.add("fade-out")
+
+    }
+
+    resetDetails(event) {
+        console.log("Reset function fired")
+        event.currentTarget.classList.remove("fade-out")
     }
 
     render() {
@@ -26,12 +49,23 @@ class Movie extends React.Component {
 
                     <img className="thumbnail" src="/assets/rogue_one_thumbnail.jpg" />
 
-                    <div className="details-box">
+                    <div className="details-box" onMouseEnter={this.fadeDetails} onMouseLeave={this.resetDetails} >
                         <div className="details-basic">
+                            <i className="fas fa-play-circle fa-3x"></i>
                             <h3 className="basics-movie-title">{title}</h3>
-
+                            <div className="details-text-container">
+                                <p className="rating">{details.maturity_rating}</p>
+                                <p className="duration">{details.duration}</p>
+                            </div>
                         </div>
-                        <video className="trailer-small" src="/assets/rogue_one_trailer" />
+
+                        <video 
+                            className="trailer-small" 
+                            src="/assets/rogue_one_trailer" 
+                            loop
+                            onMouseEnter={this.playVideo}
+                            onMouseLeave={this.stopVideo} 
+                        />
                     </div>
                 </div>
 
