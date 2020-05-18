faa93d08e2ca1ed0093f589ffa40cc3f8bf194d9
ehecker <ezhecker@gmail.com>
2020-05-18 11:00:52 -0700

    Clean code

--- frontend/components/app/movie.jsx
+++ frontend/components/app/movie.jsx
@@ -58,22 +58,18 @@ class Movie extends React.Component {
     // Fade Timing Functions
     incrementFade() {
         this.fadeTime++;
-        console.log(`Incremented to: ${this.fadeTime}`)
+
         if (this.fadeTime >= 3) {
             this.fadeInfo();
             this.fadeTime = 0;
         }
-            
     }
 
-    startFadeTimer(event) {
-        console.log("Fade timer started")
+    startFadeTimer() {
         this.fadeInterval = window.setInterval(this.incrementFade, 1000)
     }
 
-    resetFadeTimer(event) {
-        console.log("Fade timer reset")
-
+    resetFadeTimer() {
         let container = document.getElementById(`${this.props.title}-info-container`)
         container.classList.remove("trigger-fade")
 
@@ -82,14 +78,12 @@ class Movie extends React.Component {
         this.fadeInterval = window.setInterval(this.incrementFade, 1000)
     }
 
-    endFadeTimer(event) {
-        console.log("Fade Timer Ended")
+    endFadeTimer() {
         this.fadeTime = 0;
         clearInterval(this.fadeInterval)
     }
 
     fadeInfo() {
-        console.log("Fade info fired")
         let container = document.getElementById(`${this.props.title}-info-container`)
         container.classList.add("trigger-fade")
     }
@@ -118,7 +112,6 @@ class Movie extends React.Component {
 
                     <div id={`${title}-info-container`}
                     className="trailer-container" 
-                    // onMouseEnter={this.startFadeTimer}
                     onMouseMove={this.resetFadeTimer} 
                     onMouseLeave={this.endFadeTimer}
                     >
@@ -185,168 +178,3 @@ class Movie extends React.Component {
 }
 
 export default Movie;
-
-
-
-
-
-
-
-/*
-
-import React from "react";
-
-class Movie extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            hovering: false
-        }
-
-        // this.showThumbnail = this.showThumbnail.bind(this);
-        // this.hideThumbnail = this.hideThumbnail.bind(this);
-
-        this.startHover = this.startHover.bind(this);
-        this.endHover = this.endHover.bind(this);
-        this.playVideo = this.playVideo.bind(this);
-        this.pauseVideo = this.pauseVideo.bind(this);
-    }
-
-    // showThumbnail(event) {
-    //     event.preventDefault();
-    //     event.currentTarget.classList.remove("playing")
-    // }
-
-    // hideThumbnail(event) {
-    //     event.preventDefault();
-    //     event.currentTarget.classList.add("playing")
-    // }
-
-    // fadeDetails(event) {
-    //     event.currentTarget.classList.add("fade-out")
-    //     event.currentTarget.children[1].play();
-    // }
-
-    // resetDetails(event) {
-    //     event.currentTarget.classList.remove("fade-out")
-    //     event.currentTarget.children[1].pause();
-    // }
-
-    playVideo(event) {
-        debugger
-
-        console.log("Play fired")
-        event.currentTarget.children[0].play();
-    }
-
-    pauseVideo(event) {
-        debugger
-
-        console.log("Pause fired")
-        event.currentTarget.children[0].pause();
-    }
-
-    startHover() {
-        // debugger
-
-        // event.preventDefault();
-        // event.currentTarget.children[0].children[0].play();
-
-        this.setState({
-            hovering: true
-        })
-    }
-
-    endHover() {
-        // event.preventDefault();
-        // event.currentTarget.children[0].children[0].pause();
-
-        this.setState({
-            hovering: false
-        })
-    }
-
-    render() {
-        let { title, details } = this.props;
-        let { hovering } = this.state
-
-        let trailerDetails;
-        let thumbnail;
-
-        if (hovering) {
-
-            trailerDetails = (
-
-                // <div className="trailer-box">
-
-
-                //     <video 
-                //         className="trailer-small"
-                //         // src="https://spaceflix-seeds.s3-us-west-1.amazonaws.com/rogue_one_trailer.mp4"
-                //         src="/assets/rogue_one_trailer"
-                //         // src = {details.trailer}
-                //         loop
-                //         muted="muted"
-                //     />
-                // </div>
-
-                <div className="details-basic">
-                    <i className="fas fa-play-circle fa-3x"></i>
-                    <h3 className="basics-movie-title">{title}</h3>
-                    <div className="details-text-container">
-                        <p className="rating">{details.maturity_rating}</p>
-                        <p className="duration">{details.duration}</p>
-                    </div>
-                </div>
-            )
-
-        } else {
-
-            thumbnail = (
-                <img
-                    className="thumbnail" 
-                    src="/assets/rogue_one_thumbnail.jpg"
-                />
-            )
-        }
-
-        return (
-            <main className="movie-main">
-
-                <section className="movie-preview" onMouseEnter={this.startHover} onMouseLeave={this.endHover}>
-                    {thumbnail}
-
-                    <div className="trailer-box" >
-                        
-                        <video 
-                            className="trailer-small"
-                            src="/assets/rogue_one_trailer"
-                            // src="https://spaceflix-seeds.s3-us-west-1.amazonaws.com/rogue_one_trailer.mp4"
-                            // src = {details.trailer}
-                            onMouseEnter={this.playVideo}
-                            onMouseLeave={this.pauseVideo}
-
-                            loop
-                            muted="muted"
-                        />
-                        
-                        {trailerDetails}
-                    </div>
-                </section>
-
-                <section className="movie-show">
-                   // {details.description}
-                   // {details.year}
-                    // {details.director}
-                    // {details.cast}
-                </section>
-
-            </main>
-        )
-    }
-}
-
-export default Movie;
-
-*/
