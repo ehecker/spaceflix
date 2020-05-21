aa0a634e477ac933811f816b649ce33cf2f55ce0
ehecker <ezhecker@gmail.com>
2020-05-20 23:45:27 -0700

    Add watch controls functionality and styling

--- frontend/components/app/watch.jsx
+++ frontend/components/app/watch.jsx
@@ -5,8 +5,24 @@ class Watch extends React.Component {
         super(props)
 
         this.state = {
-            playing: true
+            playing: true,
+            muted: true
         }
+
+        this.togglePlay = this.togglePlay.bind(this);
+        this.toggleMute = this.toggleMute.bind(this);
+    }
+
+    togglePlay() {
+        this.setState({
+            playing: !this.state.playing
+        })
+    }
+
+    toggleMute() {
+        this.setState({
+            muted: !this.state.muted
+        })
     }
 
     render() {
@@ -14,48 +30,30 @@ class Watch extends React.Component {
         // let title = this.props.details.title;
         let title = "Rogue One: A Star Wars Story";
 
-        let { playing } = this.state; 
+        let { playing, muted } = this.state; 
         let infoOverlay;
         let playButton;
         let muteButton;
 
         if (playing) {
             playButton=(
-                <div className="watch-pause-button"></div>
+                <div className="watch-pause-btn" onClick={this.togglePlay}></div>
             )
-
-            infoOverlay=(
-                <section className="watch-controls-container">
-                    <div className="controls-top">
-                        <div className="progress-bar"></div>
-                        <p className="time-remaining"></p>
-                    </div>
-                    <div className="controls-bottom">
-                        <div className="control-btns-left">
-                            {playButton}
-                            {/* {muteButton} */}
-                            {title}
-                        </div>
-                        <div className="control-btns-right">
-                            <div className="subtitles-btn"></div>
-                            <div className="fullscreen-btn"></div>
-                        </div>
-                    </div>
-                </section>
-            )
-
         } else {
             playButton=(
-                <div className="watch-play-button"></div>
-            )
-
-            infoOverlay=(
-                <section className="watch-details-container">
-
-                </section>
+                <div className="watch-play-btn" onClick={this.togglePlay}></div>
             )
         }
 
+        if (muted) {
+            muteButton=(
+                <div className="watch-sound-off" onClick={this.toggleMute}></div>
+            )
+        } else {
+            muteButton=(
+                <div className="watch-sound-on" onClick={this.toggleMute}></div>
+            )
+        }
 
         return(
             <main className="watch-main">
@@ -63,7 +61,23 @@ class Watch extends React.Component {
 
                 </section>
                 <section className="watch-info-container">
-                    {infoOverlay}
+                    <section className="watch-controls-container">
+                        <div className="controls-top">
+                            <div className="progress-bar"></div>
+                            <p className="time-remaining"></p>
+                        </div>
+                        <div className="controls-bottom">
+                            <div className="control-btns-left">
+                                {playButton}
+                                {muteButton}
+                                {title}
+                            </div>
+                            <div className="control-btns-right">
+                                <div className="subtitles-btn"></div>
+                                <div className="fullscreen-btn"></div>
+                            </div>
+                        </div>
+                    </section>
                 </section>
             </main>
         )
